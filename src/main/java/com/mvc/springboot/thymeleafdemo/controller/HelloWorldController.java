7bfeaefe96638d6e7b646d94ce6528b961b0ba0d
mthalima <matheus.ecomp@gmail.com>
2024-10-17 22:33:33 -0300

    mais coisa simples pt.2

--- src/main/java/com/mvc/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/mvc/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -20,7 +21,7 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processForm")
+    @RequestMapping("/processFormV2")
     public String minParaMaiu(HttpServletRequest request, Model model){
         //le o parametro requisitado da form
         String theName = request.getParameter("personName");
@@ -30,6 +31,22 @@ public class HelloWorldController {
 
         //cria uma msg
         String result = "Yo! " + theName;
+
+        //add para o modelo
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV3")
+    public String processFormVersion3(@RequestParam("personName") String theName, Model model){
+
+
+        //realiza alguma coisa
+        theName = theName.toUpperCase();
+
+        //cria uma msg
+        String result = "hello my friend from v3" + theName;
 
         //add para o modelo
         model.addAttribute("message", result);
