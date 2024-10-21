4e11ad1d5776fd55359e51db7a663bc049fbba6c
mthalima <matheus.ecomp@gmail.com>
2024-10-21 20:37:15 -0300

    criado um array de paises nas properties,

--- src/main/java/com/mvc/springboot/thymeleafdemo/controller/PersonController.java
+++ src/main/java/com/mvc/springboot/thymeleafdemo/controller/PersonController.java
@@ -1,14 +1,20 @@
 package com.mvc.springboot.thymeleafdemo.controller;
 
 import com.mvc.springboot.thymeleafdemo.model.Person;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class PersonController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showPersonForm")
     public String showForm(Model theModel){
@@ -18,6 +24,9 @@ public class PersonController {
 
         //add o obj pessoa ao modelo
         theModel.addAttribute("person", pessoa);
+
+        //add a lista de paises ao modelo
+        theModel.addAttribute("countries", countries);
 
         return "person-form";
     }
