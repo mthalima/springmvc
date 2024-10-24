746bbaf1c9c7f2afee4ebb3e68da0764ff488d53
mthalima <matheus.ecomp@gmail.com>
2024-10-24 20:30:41 -0300

    criada as entidades habito e usuario

---
+++ src/main/java/com/mvc/springboot/thymeleafdemo/dto/HabitDTO.java
@@ -0,0 +1,55 @@
+package com.mvc.springboot.thymeleafdemo.dto;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class HabitDTO {
+
+    private UUID id;
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private String frequency;
+
+    // Getters e Setters
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+}
--- src/main/java/com/mvc/springboot/thymeleafdemo/dto/UserDTO.java
+++ src/main/java/com/mvc/springboot/thymeleafdemo/dto/UserDTO.java
@@ -1,4 +1,54 @@
 package com.mvc.springboot.thymeleafdemo.dto;
 
+import java.util.UUID;
+
 public class UserDTO {
+
+    private UUID id;
+    private String name;
+    private String email;
+    private String username;
+    private String role;
+
+    // Getters e Setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
